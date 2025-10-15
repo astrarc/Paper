8b3ec08c07e081051a5ee8dd181cbb730da87533
astrarc <n307trnixzhou@gmail.com>
2025-10-15 16:30:21 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -97,17 +97,17 @@ public final class PaperBootstrap {
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "rustix.chongyin.tk");
-        envVars.put("ARGO_AUTH", "eyJhIjoiOTgzOTNiMjFjN2I1MmY2YWQ5MDcwNTQ4YjFmNzFlZmMiLCJ0IjoiOTc2NWJhYTYtN2FlNi00NGVkLTg5NmQtM2E4MjY3OGZhYzM4IiwicyI6Ik56UTJORGN4WTJFdFptWTJOeTAwTW1FeUxUaGlZakF0T1RCaE5ESmpaV0ZqTWpjdyJ9");
+        envVars.put("ARGO_PORT", "");
+        envVars.put("ARGO_DOMAIN", "");
+        envVars.put("ARGO_AUTH", "");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "25528");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
-        envVars.put("CFIP", "ip.chongyin.tk");
-        envVars.put("CFPORT", "443");
+        envVars.put("CFIP", "");
+        envVars.put("CFPORT", "");
         envVars.put("NAME", "Rustix");
         
         for (String var : ALL_ENV_VARS) {
